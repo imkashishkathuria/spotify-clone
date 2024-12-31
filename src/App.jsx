6b3e25c2e9f53ec27a5463a7f374d063acb5e461
imkashishkathuria <kathuriak805@gmail.com>
2024-12-31 17:03:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
