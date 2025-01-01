94dde8502621409a2bb86c6db1c963dfb0b61469
imkashishkathuria <kathuriak805@gmail.com>
2025-01-01 22:29:49 +0530

    completed sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import SideBar from "./components/SideBar"
 
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
-        
+        <SideBar />
       </div>
 
     </div>
