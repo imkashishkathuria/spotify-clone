1d4fc7a9782d67081c3d689f23a067be4e0fa2c0
imkashishkathuria <kathuriak805@gmail.com>
2025-01-03 22:09:01 +0530

    music album page

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom"
 
 const AlbumItem = ({name, image, desc, id}) => {
+  const navigate = useNavigate();
   return (
-    <div className="min-w-[100px] p-1 px-5 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div onClick={()=>navigate(`/album/${id}`)} className="min-w-[100px] p-1 px-5 rounded cursor-pointer hover:bg-[#ffffff26]">
         <img className="rounded" src={image} alt=""/>
         <p className="font-semibold mt-1 mb-1">{name}</p>
         <p className="text-slate-200 text-[11px]">{desc}</p>
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom"
 
 const SongItem = ({name, desc, image, id}) => {
+  const navigate = useNavigate();
   return (
-    <div className="min-w-[100px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div onClick={()=>navigate(`/song/${id}`)} className="min-w-[100px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
     <img className="rounded" src={image} alt=""/>
     <p className="font-semibold mt-1 mb-1">{name}</p>
     <p className="text-slate-200 text-[11px]">{desc}</p>
